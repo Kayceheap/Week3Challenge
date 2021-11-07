56a4efa609ef76c6c527925c62ac4f5af202ec1f
Kayce Heap <kayce.heap@gmail.com>
2021-11-06 22:38:07 -0600

    added pseudo code to the js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,15 @@
 // Assignment code here
-
+// Step 1. prompt user for password length
+// Step 2. Verify length is between 8 - 128
+// Step 3. If length is invalid warn user and re-prompt
+// Step 4. Ask user if they want lower case letters
+// Step 5. Ask user if they want upper case letters
+// Step 6. Ask user if they want numeric
+// Step 7. Ask user if they want special characters
+// Step 8. Verify that at least one group was selected
+// Step 9. If no group was selected then warn user and re-prompt
+// Step 10. Generate a password with the criteria
+// Step 11. Password is shown as an alert or on the webpage
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
